22b600556bc983bfeb6a8dfb8200e49e0b14a475
Michel Keijzers <michelkeijzers@hotmail.com>
2026-03-15 14:47:49 +0100

    part 2

---
+++ main/slaves/webserver_slave/dmx_controller/WebserverSlave.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "DmxControllerWebserverSlave.hpp"
+
+// Note: WebserverSlave is now implemented as DmxControllerWebserverSlave
+// See DmxControllerWebserverSlave.hpp for the class definition
+// This file is kept for backward compatibility with existing includes
